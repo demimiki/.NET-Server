437ee9602fb766b53225820683198ddb07995e5d
DEMiMiKi <55283338+demimiki@users.noreply.github.com>
2019-12-08 15:51:07 +0100

    Add files via upload

---
+++ Controllers/PersonController.cs
@@ -0,0 +1,46 @@
+﻿using ASPNetCoreWebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ASPNetCoreWebAPI.Controllers
+{
+    [ApiController]
+    [Route("person")]
+    public class PersonController : ControllerBase
+    {
+        static public List<Person> people = new List<Person>();
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Person>> Get()
+        {
+            return Ok(people);
+        }
+
+        [HttpGet("{id:int}")]
+        public ActionResult<Person> Get(int id)
+        {
+            if(id>=0 && id < people.Count)
+            {
+                return Ok(people[id]);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody]Person person)
+        {
+            people.Add(person);
+            return Ok();
+        }
+
+        [HttpDelete]
+        public ActionResult Delete(int id)
+        {
+            people.RemoveAt(id);
+            return Ok();
+        }
+
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASPNetCoreWebAPI.Models
+{
+    public class Person
+    {
+        public string Nev { get; set; }
+        public string Lakcim { get; set; }
+        public string Tajszam { get; set; }
+        public string Panasz { get; set; }
+        public DateTime Idopont { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Nev} {Tajszam} {Idopont:yyyy.MM.dd. hh:mm:ss}";
+        }
+    }
+}
